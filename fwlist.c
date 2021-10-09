a490a37083a367f9acbec6b5d9fd527efc78c7bd
dartax009 <dartax009@gmail.com>
2021-10-09 20:30:11 +0300

    Добавлены новые функции для работы со списом

--- fwlist.c
+++ fwlist.c
@@ -21,7 +21,10 @@ uint8_t pop (s_mas **head, void *val, const size_t el)
 	s_mas *tmp = NULL;
 
 	tmp = (*head);
-	memcpy(val, tmp->value, el);
+
+	if (val != 0)
+		memcpy(val, tmp->value, el);
+
 	(*head) = (*head)->next;
 
 	free(tmp->value);
@@ -32,6 +35,9 @@ uint8_t pop (s_mas **head, void *val, const size_t el)
 
 s_mas *findN (s_mas *head, const uint64_t n)
 {
+	if (head == NULL)
+		return NULL;
+
 	for (uint64_t i=0; i<n; i++)
 	{
 		head = head->next;
@@ -41,7 +47,7 @@ s_mas *findN (s_mas *head, const uint64_t n)
 	return head;
 }
 
-uint8_t pushN (s_mas *head, const uint32_t n, const void *val, const size_t el)
+uint8_t setN (s_mas *head, const uint64_t n, const void *val, const size_t el)
 {
 	head = findN(head, n);
 	if (head == NULL)
@@ -102,10 +108,50 @@ uint8_t popBack (s_mas **head, void *val, const size_t el)
 	while (tmp->next->next != NULL)
 		tmp = tmp->next;
 
-	memcpy(val, tmp->next->value, el);
+	if (val != 0)
+		memcpy(val, tmp->next->value, el);
 
 	free(tmp->next->value);
 	free(tmp->next);
 	tmp->next = NULL;
+	return 0;
+}
+
+uint8_t pushN (s_mas *head, const uint64_t n, const void *val, const size_t el)
+{
+	head = findN(head, n);
+
+	if (head == NULL)
+		return 1;
+
+	s_mas *tmp = (s_mas *) malloc(sizeof(s_mas));
+	tmp->value = malloc(el);
+	memcpy(tmp->value, val, el);
+
+	tmp->next = head->next;
+
+	head->next = tmp;
+
+	return 0;
+}
+
+uint8_t popN (s_mas **head, const uint64_t n, void *val, const size_t el)
+{
+	if (n == 0)
+		return pop(head, val, el);
+
+	s_mas *ahead = findN( (*head), n-1);
+	if (ahead == NULL || ahead->next == NULL)
+		return 1;
+
+	s_mas *tmp = ahead->next;
+
+	if (val != 0)
+		memcpy(val, tmp->value, el);
+
+	ahead->next = tmp->next;
+	free(tmp->value);
+	free(tmp);
+
 	return 0;
 }
